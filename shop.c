09a00c26e9e4568fe0da1237bfd2f7472c395471
rastislp <g00252861@gmit.ie>
2020-10-31 18:02:17 +0000

    choice 2 customer shopping list display completed

--- shop.c
+++ shop.c
@@ -41,16 +41,20 @@ void printProduct(struct Product p)
 
 void printCustomer(struct Customer c)
 {
-	printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
-	printf("-------------\n");
-	for(int i = 0; i < c.index; i++)
-	{
-		printProduct(c.shoppingList[i].product);
-		printf("%s ORDERS %d OF ABOVE PRODUCT\n", c.name, c.shoppingList[i].quantity);
-		double cost = c.shoppingList[i].quantity * c.shoppingList[i].product.price; 
-		printf("The cost to %s will be €%.2f\n", c.name, cost);
-	}
-}
+   double totalCost = 0.0;
+   printf("--------------------------------\n");
+   printf("| Name of customer: %s      |\n",  c.name);
+   printf("| %s's budget: %.2f        |\n", c.name,  c.budget);
+   printf("--------------------------------\n");
+   printf("| Product             |  Q-ty  |\n");
+   for(int i =0; i<c.index; i++)
+   {
+      double lineCost = c.shoppingList[i].quantity * c.shoppingList[i].product.price;
+      printf("| %-20.20s|   %.2d   |\n", c.shoppingList[i].product.name, c.shoppingList[i].quantity);
+      
+   };
+   printf("--------------------------------\n");
+};
 
 struct Shop createAndStockShop()
 {
@@ -67,9 +71,8 @@ struct Shop createAndStockShop()
     double cashInShop = atof(line);
 	struct Shop shop = { cashInShop };
 
-    while ((read = getline(&line, &len, fp)) != -1) {
-        // printf("Retrieved line of length %zu:\n", read);
-        // printf("%s IS A LINE", line);
+    while ((read = getline(&line, &len, fp)) != -1) 
+	{
 		char *n = strtok(line, ",");
 		char *p = strtok(NULL, ",");
 		char *q = strtok(NULL, ",");
@@ -80,10 +83,42 @@ struct Shop createAndStockShop()
 		struct Product product = { name, price };
 		struct ProductStock stockItem = { product, quantity };
 		shop.stock[shop.index++] = stockItem;
-		// printf("NAME OF PRODUCT %s PRICE %.2f QUANTITY %d\n", name, price, quantity);
     }
 	
 	return shop;
+}
+struct Customer selectCustomer (struct Shop s, char* csvFile)
+{
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(csvFile, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+	
+	getline(&line, &len, fp);
+	char* cn = strtok(line, ",");
+    char* cb = strtok(NULL, ",");
+    char* name = malloc(sizeof(char) * 50);
+    strcpy(name,cn);
+    double budget = atof(cb);
+	struct Customer customer = { name,budget };
+
+    while ((read = getline(&line, &len, fp)) != -1)
+	{
+		char *pn = strtok(line, ",");
+		char *qn = strtok(NULL, ",");
+		int quantity = atoi(qn);
+		char *prodName = malloc(sizeof(char) * 50);
+		strcpy(prodName, pn);
+		struct Product product = { prodName, quantity };
+		struct ProductStock listItem = { product, quantity };
+		customer.shoppingList[customer.index++] = listItem;
+    }
+	
+	return customer;
 }
 
 void printShop(struct Shop s)
@@ -99,11 +134,13 @@ void printShop(struct Shop s)
 	}
 }
 
+
 void menu(struct Shop s)
 {
 	int choice = 0;
+	char customerFile;
 	
-	
+	printf("\n\n");
 	printf("------------------------------------------------------\n");
 	printf("|             Welcome in Shop application            |\n");
     printf("------------------------------------------------------\n");
@@ -117,14 +154,22 @@ void menu(struct Shop s)
     printf("3 Go to checkout\n");
     printf("\n");
     printf("4 Exit \n");
+	printf("\n\nType your choice here >>> ");
 	scanf("%d", &choice);
     if(choice == 1)
 	{
 		printShop(s);
+		menu(s);
+		
 	}
 	else if(choice == 2)
 	{
-		exit(0);
+	  printf("\n\nFor displaying customer list please type customer file name in following format:	customer1.csv , customer2.csv ...\n");
+	  printf("File name >>> ");
+	  scanf("%s", &customerFile);
+      struct Customer newCustomer = selectCustomer(s, &customerFile);
+      printCustomer(newCustomer); 
+	  menu(s);	
 	}		
 	else if(choice == 3)
 	{
@@ -141,6 +186,7 @@ void menu(struct Shop s)
 int main(void) 
 {   // Create a shop stock
 	struct Shop shop = createAndStockShop();
+	
 	// Display application menu
 	menu(shop);
 	
